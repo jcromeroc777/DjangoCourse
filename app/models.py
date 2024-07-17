2133a48f1cf489a771d94ba99e3d833b074faf4c
Julio Romero <jcromeroc.19@gmail.com>
2024-07-17 09:45:38 -0400

    feat: example de car finish

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Cars(models.Model):
     brand = models.TextField(max_length=250)
+    year = models.TextField(max_length=4, null=True)
 
     def __str__(self):
-        return self.name
+        return f'{self.brand} - {self.year}'
